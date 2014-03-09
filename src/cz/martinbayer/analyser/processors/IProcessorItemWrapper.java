180383c7e2b763b661479a90e0995bcfb906acb7
Martin Bayer <martin.bay016@gmail.com>
2014-03-09 16:17:40 +0100

    doubleclick for processors implemented

--- src/cz/martinbayer/analyser/processors/IProcessorItemWrapper.java
+++ src/cz/martinbayer/analyser/processors/IProcessorItemWrapper.java
@@ -1,10 +1,14 @@
 package cz.martinbayer.analyser.processors;
 
+import org.eclipse.swt.events.MouseEvent;
+
 import cz.martinbayer.analyser.processors.model.IXMLog;
 
 public interface IProcessorItemWrapper<T extends IXMLog> {
 	IProcessorLogic<T> getProcessorLogic();
-	
+
 	IProcessorsPaletteItem getProcessorPaletteItem();
-	
+
+	void mouseDoubleClicked(MouseEvent e);
+
 }
